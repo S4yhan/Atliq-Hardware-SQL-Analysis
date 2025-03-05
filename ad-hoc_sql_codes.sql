b706d69d76a480a88f30bdf8bfd907c14880d250
Sayhan Sameer <153631099+S4yhan@users.noreply.github.com>
2025-03-05 17:03:58 +0530

    Update ad-hoc_sql_codes.sql

--- ad-hoc_sql_codes.sql
+++ ad-hoc_sql_codes.sql
@@ -1,6 +1,3 @@
-Here is the organized version of your SQL file:
-
-```sql
 -- 1. Provide the list of markets in which customer "Atliq Exclusive" operates its business in the APAC region
 SELECT DISTINCT market
 FROM dim_customer
